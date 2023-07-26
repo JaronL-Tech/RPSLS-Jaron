514e2f1bdc0392247ec09561030fa51b767bad49
Jaron Leach <84044616+JaronL-Tech@users.noreply.github.com>
2023-07-26 10:00:42 -0400

    Updated and curated the code to be more precise

---
+++ RPSLS_FSF_CSharp_Starter-main/RPSLS/ComputerPlayerclass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal abstract class ComputerPlayerclass
+    {
+        //Member Variabes (HAS A)
+        public string name;
+        public List<string> gestures;
+        public string chosenGesture;
+        public int score;
+
+        //Constructor
+        public ComputerPlayerclass(string name)
+        {
+            this.name = name;
+            gestures = new List<string> { "rock", "paper", "scissors", "lizard", "Spock" };
+            chosenGesture = "";
+            score = 0;
+        }
+
+        //Member Methods (CAN DO)
+        //This abstract method must be overridden by the child Player classes
+        public abstract void ChooseGesture();
+        public class Computerplayer : Player
+        {
+            public Computerplayer(string name) : base(name) { }
+            public override void ChooseGesture()
+            {
+                Console.WriteLine($"The Ai will now pick a Gesture");
+                Random random = new Random();
+                int randomIndex = random.Next(gestures.Count);
+                chosenGesture = gestures[randomIndex];
+            }
+
+
+
+
+
+
+
+        } 
+        
+        
+        
+      
+         
+    }
+}
---
+++ RPSLS_FSF_CSharp_Starter-main/RPSLS/Game.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class Game
+    {
+        //Member Variabes (HAS A)
+        public Player playerOne;
+        public Player playerTwo;
+
+        //Constructor
+        public Game()
+        {
+
+        }
+
+        //Member Methods (CAN DO)
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to RPSLS!");
+            Console.WriteLine("This game is a true test of skills and strategy ");
+            Console.WriteLine("The rules are simple Rock crushes Scissors\nScissors cuts Paper \nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n ");
+            Console.WriteLine("The first competitor to reach 3 points wins the game!");
+        }
+
+        public int ChooseNumberOfHumanPlayers() 
+        {
+            int numbersofplayers;
+            Console.WriteLine("How many Players? (Enter 1 or 2)");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out numbersofplayers))
+                {
+                    if (numbersofplayers == 1 || numbersofplayers == 2)
+                    {
+                        return numbersofplayers;
+                    }
+
+                }
+            }
+        }
+
+
+
+
+
+
+        public void CreatePlayerObjects(int numberOfHumanPlayers)
+
+        { 
+        }
+        private Player player1;
+        private Player player2;
+
+        public Game(Player player1, Player player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public void CompareGestures()
+        {
+            Console.WriteLine($"{player1.name} chose {player1.chosenGesture}");
+            Console.WriteLine($"{player2.name} chose {player2.chosenGesture}");
+
+            int result = CompareGestures(player1.chosenGesture, player2.chosenGesture);
+
+            if (result == 1)
+            {
+                Console.WriteLine($"{player1.name} wins the round!");
+                player1.score++;
+            }
+            else if (result == -1)
+            {
+                Console.WriteLine($"{player2.name} wins the round!");
+                player2.score++;
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
+        }
+
+        private int CompareGestures(string gesture1, string gesture2)
+        {
+            if (gesture1 == gesture2)
+            {
+                return 0; // Tie
+            }
+            else if (
+                (gesture1 == "rock" && (gesture2 == "scissors" || gesture2 == "lizard")) ||
+                (gesture1 == "paper" && (gesture2 == "rock" || gesture2 == "Spock")) ||
+                (gesture1 == "scissors" && (gesture2 == "paper" || gesture2 == "lizard")) ||
+                (gesture1 == "lizard" && (gesture2 == "Spock" || gesture2 == "paper")) ||
+                (gesture1 == "Spock" && (gesture2 == "scissors" || gesture2 == "rock"))
+            )
+            {
+                return 1; // Player 1 wins
+            }
+            else
+            {
+                return -1; // Player 2 wins
+            }
+        }
+
+        public void DisplayGameWinner()
+        {
+            if (player1.score >= 2)
+            {
+                Console.WriteLine($"{player1.name} wins the game!");
+            }
+            else if (player2.score >= 2)
+            {
+                Console.WriteLine($"{player2.name} wins the game!");
+            }
+            else
+            {
+                Console.WriteLine("The game is still ongoing. No winner yet.");
+            }
+        }
+
+        public void RunGame()
+        {
+            WelcomeMessage();
+            ChooseNumberOfHumanPlayers();
+            CompareGestures();
+            DisplayGameWinner();
+        }
+    }
+}
