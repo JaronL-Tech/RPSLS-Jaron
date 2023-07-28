90a8e06f0b3f8f26b967d256414336eb490d1ad3
Jaron <Jaronleach98@gmail.com>
2023-07-28 10:36:03 -0400

    Fixed and edited the player and computer classes to be more inilgined with intended vision of the assignment.

--- RPSLS/ComputerPlayerclass.cs
+++ RPSLS/ComputerPlayerclass.cs
@@ -7,26 +7,7 @@ using System.Threading.Tasks;
 
 namespace RPSLS
 {
-    internal abstract class ComputerPlayerclass
-    {
-        //Member Variabes (HAS A)
-        public string name;
-        public List<string> gestures;
-        public string chosenGesture;
-        public int score;
-
-        //Constructor
-        public ComputerPlayerclass(string name)
-        {
-            this.name = name;
-            gestures = new List<string> { "rock", "paper", "scissors", "lizard", "Spock" };
-            chosenGesture = "";
-            score = 0;
-        }
-
-        //Member Methods (CAN DO)
-        //This abstract method must be overridden by the child Player classes
-        public abstract void ChooseGesture();
+  
         public class Computerplayer : Player
         {
             public Computerplayer(string name) : base(name) { }
@@ -44,11 +25,5 @@ namespace RPSLS
 
 
 
-        } 
-        
-        
-        
-      
-         
-    }
+        }   
 }
